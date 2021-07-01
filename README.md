dce78d57a8ca6b08f7b7efcdfc6ab96c7e1e7405
Mingyang-Li <mingyangli118@gmail.com>
2021-07-01 16:17:51 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Apollo API using TypeScript and Prisma ORM
+
+### Tutorial link: https://dev.to/prisma/prototyping-a-crud-api-with-typegraphql-and-prisma-for-your-database-424c
+
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import * as tq from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+import { resolvers } from "@generated/type-graphql";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+const app = async () => {
+  const schema = await tq.buildSchema({ resolvers })
+
+  const context = {
+    prisma
+  }
+
+  new ApolloServer({ schema, context }).listen({ port: 4000 }, () =>
+    console.log('🚀 Server ready at: http://localhost:4000')
+  )
+}
+
+app()
